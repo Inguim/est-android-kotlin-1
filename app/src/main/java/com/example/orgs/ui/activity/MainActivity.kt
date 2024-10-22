a291fa1e872f6ff4a870800f8b143d62b7230180
Inguim <igorazevedost@gmail.com>
2024-10-22 17:53:57 -0300

    feat: RecyleView para lista de produtos

---
+++ app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.orgs.ui.activity
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.orgs.R
+import com.example.orgs.model.Produto
+import com.example.orgs.ui.recyclerView.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = ListaProdutosAdapter(
+            context = this, produtos = listOf(
+                Produto("Manga", "Mangá of Zé", BigDecimal("19.99")),
+                Produto("Uva", "Uva of Zé", BigDecimal("10.99")),
+            )
+        )
+    }
+}
---
+++ app/src/main/java/com/example/orgs/ui/recyclerView/adapter/ListaProdutosAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.orgs.ui.recyclerView.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.orgs.R
+import com.example.orgs.model.Produto
+
+// RecyclerView: responsável por exibir informações em lista
+class ListaProdutosAdapter(
+    private val context: Context,
+    private val produtos: List<Produto>
+) : RecyclerView.Adapter<ListaProdutosAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun vincula(produto: Produto) {
+            val nome = itemView.findViewById<TextView>(R.id.nome)
+            nome.text = produto.nome
+            val descricao = itemView.findViewById<TextView>(R.id.descricao)
+            descricao.text = produto.descricao
+            val valor = itemView.findViewById<TextView>(R.id.valor)
+            valor.text = produto.valor.toPlainString()
+        }
+    }
+
+    // Responsável por criar a View e efetuar o processo de "Bind" (vinculação de dado e ui)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.produto_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    // Indica o item, posição e o ViewHolder que do mesmo
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val produto = produtos[position]
+        holder.vincula(produto)
+    }
+
+    // Representa a quantidade de items que o mesmo irá representar
+    override fun getItemCount(): Int = produtos.size
+}

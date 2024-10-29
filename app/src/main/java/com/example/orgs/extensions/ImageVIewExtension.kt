21c1e42b996e648ca8a4a99977c6fd66d1244d26
Inguim <igorazevedost@gmail.com>
2024-10-29 11:30:05 -0300

    fear: foi adicionada uma Extension ao ImageView gerar ImageLoader

--- app/src/main/java/com/example/orgs/extensions/ImageVIewExtension.kt
+++ app/src/main/java/com/example/orgs/extensions/ImageVIewExtension.kt
@@ -1,7 +1,11 @@
 package com.example.orgs.extensions
 
+import android.content.Context
+import android.os.Build.VERSION.SDK_INT
 import android.widget.ImageView
 import coil.ImageLoader
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
 import coil.load
 import com.example.orgs.R
 
@@ -11,4 +15,15 @@ fun ImageView.carregar(url: String? = null, imageLoader: ImageLoader) {
         error(R.drawable.erro)
         placeholder(R.drawable.placeholder)
     }
+}
+
+fun ImageView.gerarImageLoader(context: Context): ImageLoader {
+    return ImageLoader.Builder(context)
+        .componentRegistry {
+            if (SDK_INT >= 28) {
+                add(ImageDecoderDecoder(context))
+            } else {
+                add(GifDecoder())
+            }
+        }.build()
 }

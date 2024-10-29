86a613e0515f686219aa0df14ee6728c8c441252
Inguim <igorazevedost@gmail.com>
2024-10-28 21:51:41 -0300

    fear: foi adicionado o formulario_imagem.xml e sua lógica de invocação

--- app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -1,7 +1,9 @@
 package com.example.orgs.ui.activity
 
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.example.orgs.R
 import com.example.orgs.dao.ProdutosDao
 import com.example.orgs.databinding.ActivityFormularioProdutoBinding
 import com.example.orgs.model.Produto
@@ -16,6 +18,13 @@ class FormularioProdutoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         configurarBotaoSalvar()
+        binding.activityFormularioProdutoImagem.setOnClickListener() {
+            AlertDialog.Builder(this)
+                .setView(R.layout.formulario_imagem)
+                .setPositiveButton("Confirmar") { _, _ -> }
+                .setNegativeButton("Cancelar") { _, _ -> }
+                .show()
+        }
     }
 
     private fun configurarBotaoSalvar() {
@@ -29,11 +38,11 @@ class FormularioProdutoActivity : AppCompatActivity() {
     }
 
     private fun criarProduto(): Produto {
-        val campoNome = binding.produtoItemNome
+        val campoNome = binding.activityFormularioProdutoNome
         val nome = campoNome.text.toString()
-        val campoDescricao = binding.produtoItemDescricao
+        val campoDescricao = binding.activityFormularioProdutoDescricao
         val descricao = campoDescricao.text.toString()
-        val campoValor = binding.produtoItemValor
+        val campoValor = binding.activityFormularioProdutoValor
         val valorTexto = campoValor.text.toString()
         val valor = if (valorTexto.isBlank()) BigDecimal.ZERO else BigDecimal(valorTexto)
         return Produto(nome, descricao, valor)

d7316128f3d7fa93002678614d111b525604de5e
Inguim <igorazevedost@gmail.com>
2024-10-29 14:32:39 -0300

    feat: foi mudado o tema de cores

--- app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -20,6 +20,7 @@ class FormularioProdutoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        title = "Cadastrar produto"
         configurarBotaoSalvar()
         binding.activityFormularioProdutoImagem.setOnClickListener() {
             val imageLoader = binding.activityFormularioProdutoImagem.gerarImageLoader(this)

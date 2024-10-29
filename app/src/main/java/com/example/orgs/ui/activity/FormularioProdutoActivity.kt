c13653e2a60c2ae7abacbf19b92b04e68bbe1271
Inguim <igorazevedost@gmail.com>
2024-10-29 16:11:39 -0300

    feat: adicionado validação FormularioProdutoActivity

--- app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -2,12 +2,14 @@ package com.example.orgs.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.orgs.R
 import com.example.orgs.dao.ProdutosDao
 import com.example.orgs.databinding.ActivityFormularioProdutoBinding
 import com.example.orgs.extensions.carregar
 import com.example.orgs.extensions.gerarImageLoader
 import com.example.orgs.model.Produto
 import com.example.orgs.ui.dialog.FormularioImagemDialog
+import com.google.android.material.textfield.TextInputEditText
 import java.math.BigDecimal
 
 class FormularioProdutoActivity : AppCompatActivity() {
@@ -20,7 +22,7 @@ class FormularioProdutoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        title = "Cadastrar produto"
+        title = getString(R.string.app_formulario_produtos_title)
         configurarBotaoSalvar()
         binding.activityFormularioProdutoImagem.setOnClickListener() {
             val imageLoader = binding.activityFormularioProdutoImagem.gerarImageLoader(this)
@@ -35,9 +37,11 @@ class FormularioProdutoActivity : AppCompatActivity() {
         val botaoSalvar = binding.activityFormularioProdutoBotaoSalvar
         val dao = ProdutosDao()
         botaoSalvar.setOnClickListener {
-            val produtoNovo = criarProduto()
-            dao.adicionar(produtoNovo)
-            finish()
+            if (!validarDado()) {
+                val produtoNovo = criarProduto()
+                dao.adicionar(produtoNovo)
+                finish()
+            }
         }
     }
 
@@ -50,5 +54,45 @@ class FormularioProdutoActivity : AppCompatActivity() {
         val valorTexto = campoValor.text.toString()
         val valor = if (valorTexto.isBlank()) BigDecimal.ZERO else BigDecimal(valorTexto)
         return Produto(nome, descricao, valor, url)
+    }
+
+    private fun validarDado(): Boolean {
+        var error = false
+        var campo: TextInputEditText? = null
+        val nomeValido = validarCampoNome()
+        val descricaoValido = validarCampoDescricao()
+        val valorValido = validarCampoValor()
+        if (!nomeValido || !descricaoValido || !valorValido) {
+            error = true
+        }
+        if (!valorValido) {
+            campo = binding.activityFormularioProdutoValor
+            campo.error = getString(R.string.formulario_produto_error_valor)
+        }
+        if (!descricaoValido) {
+            campo = binding.activityFormularioProdutoDescricao
+            campo.error = getString(R.string.formulario_produto_error_descricao)
+        }
+        if (!nomeValido) {
+            campo = binding.activityFormularioProdutoNome
+            campo.error = getString(R.string.formulario_produto_error_nome)
+        }
+        if (error) campo?.requestFocus()
+        return error
+    }
+
+    private fun validarCampoNome(): Boolean {
+        val campo = binding.activityFormularioProdutoNome
+        return campo.text.toString().isNotBlank()
+    }
+
+    private fun validarCampoDescricao(): Boolean {
+        val campo = binding.activityFormularioProdutoDescricao
+        return campo.text.toString().isNotBlank()
+    }
+
+    private fun validarCampoValor(): Boolean {
+        val campo = binding.activityFormularioProdutoValor
+        return campo.text.toString().isNotBlank()
     }
 }

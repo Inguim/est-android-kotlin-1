836f51f1cfa26d4942b3b235675bf3fcd4757598
Inguim <igorazevedost@gmail.com>
2024-12-03 14:14:49 -0300

    feat: adicionado StringExtension para gerar Hash

---
+++ app/src/main/java/com/example/orgs/extensions/StringExtension.kt
@@ -0,0 +1,14 @@
+package com.example.orgs.extensions
+
+import java.security.MessageDigest
+
+fun String.toHash(
+    algoritmo: String = "SHA-256"
+): String {
+    return MessageDigest
+        .getInstance(algoritmo)
+        .digest(this.toByteArray())
+        .fold("") { str, byte ->
+            str + "%02x".format(byte)
+        }
+}
